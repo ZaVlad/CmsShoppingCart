7d0a52ad76fc5667bfadac6d0d0b986637c7e93f
Admin <Admin@DESKTOP-FHS7GO2>
2021-03-04 20:25:34 +0200

    Add some new features

--- CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
+++ CmsShoppingCart/Areas/Admin/Controllers/PagesController.cs
@@ -1,5 +1,6 @@
 ﻿using CmsShoppingCart.Infrastructure;
 using CmsShoppingCart.Models;
+using Microsoft.AspNetCore.Http;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.EntityFrameworkCore;
 using System;
@@ -17,13 +18,115 @@ namespace CmsShoppingCart.Areas.Admin.Controllers
         {
             _context = cmsShoppingCartDbContext;
         }
-        [Route("[area]/[controller]")]
-        public async Task<IActionResult> Index()
+        [Route("[area]/[controller]/{Id?}",Name = "Index")]
+        // Get /admin/pages
+        public async Task<IActionResult> IndexAsync()
         {
             IQueryable<Page> pages = _context.Pages.OrderBy(p=>p.Sorting);
             var pagesList = await pages.ToListAsync();
 
             return View(pagesList);
+
+        }
+        [Route("[area]/[controller]/[action]/{Id}")]
+        public async Task<IActionResult> DetailsAsync(int Id)
+        {
+            Page page = await _context.Pages.FirstOrDefaultAsync(s => s.Id == Id);
+            if(page == null)
+            {
+                return StatusCode(StatusCodes.Status404NotFound);
+            }
+
+            return View(page);
+
+        }
+        [Route("[area]/[controller]/[action]")]
+        public  IActionResult Create()
+        {
+            return View();
+
+        }
+        [Route("[area]/[controller]/[action]")]
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public async Task<IActionResult> CreateAsync(Page page)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(page);
+            }
+            page.Slug = page.Title.ToLower().Replace(" ", "-");
+            page.Sorting = 100;
+
+            var slug = await _context.Pages.FirstOrDefaultAsync(s => s.Slug == page.Slug);
+            if(slug != null)
+            {
+                ModelState.AddModelError("","The page already exists");
+                return View(page);
+            }
+
+             _context.Add(page);
+            await _context.SaveChangesAsync();
+            TempData["Success"] = "The page has been Created";
+            return RedirectToAction("Index");
+
+        }
+
+        [Route("[area]/[controller]/[action]/{Id}")]
+        public async Task<IActionResult> EditAsync(int Id)
+        {
+            Page page = await _context.Pages.FindAsync(Id);
+            if (page == null)
+            {
+                return StatusCode(StatusCodes.Status404NotFound);
+            }
+
+            return View(page);
+
+        }
+
+       
+        [Route("[area]/[controller]/[action]/{Id}")]
+        [HttpPost]
+        public async Task<IActionResult> EditAsync(Page page)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View(page);
+            }
+            page.Slug = page.Id == 1 ? "home" : page.Title.ToLower().Replace(" ", "-");
+            var slug = await _context.Pages.Where(s =>s.Id!=page.Id).FirstOrDefaultAsync(s => s.Slug == page.Slug);
+            if (slug != null)
+            {
+                ModelState.AddModelError("", "The page already exists");
+                return View(page);
+            }
+
+            _context.Update(page);
+            await _context.SaveChangesAsync();
+            TempData["Success"] = "The page has been edited";
+            return RedirectToAction("Edit",new { Id = page.Id});
+
+        }
+
+        [Route("[area]/[controller]/[action]/{Id}")]
+        public async Task<IActionResult> DeleteAsync(int Id)
+        {
+            Page page = await _context.Pages.FindAsync(Id);
+            if (page == null)
+            {
+                TempData["Error"] = "The page has not exists";
+            }
+            else
+            {
+                _context.Pages.Remove(page);
+                await _context.SaveChangesAsync();
+                TempData["Success"] = "The page has already removed";
+            }
+
+            return RedirectToAction("Index");
+
+
 
         }
     }
--- CmsShoppingCart/Models/Page.cs
+++ CmsShoppingCart/Models/Page.cs
@@ -9,11 +9,12 @@ namespace CmsShoppingCart.Models
     public class Page
     {
         public int Id { get; set; }
-        [Required]
+        [Required,]
+        [MaxLength(10),MinLength(2, ErrorMessage = "Minimum Length is 2")]
         public string Title { get; set; }
-        [Required]
         public string Slug { get; set; }
         [Required]
+        [MaxLength(100), MinLength(4, ErrorMessage = "Minimum Length is 4")]
         public string Content { get; set; }
         public int Sorting { get; set; }
 
